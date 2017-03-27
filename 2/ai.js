9e24bd4b5839a1c3dfa4a5ec3f4a46ce40606942
PochWar <ripoche.b@gmail.com>
2017-03-27 20:04:00 +0200

    Manage dumbbots move independently

--- 2/ai.js
+++ 2/ai.js
@@ -1,31 +1,38 @@
-//bot move
-var goTop = true;
-var goBottom = true;
-var goLeft = true;
-var goRight = true;
+var goTop = {};
+var goBottom = {};
+var goLeft = {};
+var goRight = {};
+//set "go" directions for each dumbbot
+dumbBot.forEach(function(dumbBot){
+goTop[dumbBot.id] = true;
+goBottom[dumbBot.id] = true;
+goLeft[dumbBot.id] = true;
+goRight[dumbBot.id] = true;
+});
+//move bot
 function moveDumbBot(){
     dumbBot.forEach(function(dumbBot){
         // console.log(dumbBot.move("bottom"))
-        if(goRight){
+        if(goRight[dumbBot.id]){
             if(!dumbBot.move("right")){
-                goRight = false;
+                goRight[dumbBot.id] = false;
             }
         } else {
-            if(goBottom){
+            if(goBottom[dumbBot.id]){
                 if(!dumbBot.move("bottom")){
-                    goBottom = false;
+                    goBottom[dumbBot.id] = false;
                 }
             } else {
-                if(goLeft){
+                if(goLeft[dumbBot.id]){
                     if(!dumbBot.move("left")){
-                        goLeft = false;
+                        goLeft[dumbBot.id] = false;
                     }
                 } else {
-                    if(goTop){
+                    if(goTop[dumbBot.id]){
                         if(!dumbBot.move("top")){
-                            goRight = true;
-                            goBottom = true;
-                            goLeft = true;
+                            goRight[dumbBot.id] = true;
+                            goBottom[dumbBot.id] = true;
+                            goLeft[dumbBot.id] = true;
                         }
                     }
                 }
