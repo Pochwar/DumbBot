293213d530d1f40ff5c936f3afc2459ce66fb8ce
PochWar <ripoche.b@gmail.com>
2017-04-10 01:15:40 +0200

    Optimize ai-zombie

--- ai-zombie.js
+++ ai-zombie.js
@@ -1,78 +1,39 @@
-var go = {};
-go["top"] = {};
-go["bottom"] = {};
-go["left"] = {};
-go["right"] = {};
-
-
-
 //move dumbBot
 function moveDumbBot(){
     //make bot rush closest player
     items.dumbBot.forEach(function(dumbBot){
-        var playerTop = [];
-        var playerLeft = [];
-        var closestTop ={};
-        var closestLeft = {};
-        closestTop.dist = undefined;
-        closestLeft.dist = undefined;
+        var closestPlayer = {};
         var distanceTemp = Infinity;
 
-
-        items.player.forEach(function(player){
-            //put players coords in arrays
-            playerTop.push(removePxParseInt(document.querySelector("#"+player.id).style.top));
-            playerLeft.push(removePxParseInt(document.querySelector("#"+player.id).style.left));
-
-        })
-        //check wich player is closest
-        //top
-        playerTop.forEach(function(playerTop, i){
-            //first player : set distanceTop.value and distanceTop.id
-            if (closestTop.dist === undefined){
-                closestTop.dist = Math.abs(dumbBot.top - playerTop);
-                closestTop.id = i;
+        //determines wich player is the closest
+        items.player.forEach(function(player, i){
+            //get top and left foreach player
+            var playerTop = removePxParseInt(document.querySelector("#"+player.id).style.top);
+            var playerLeft = removePxParseInt(document.querySelector("#"+player.id).style.left);
+            //claculate distance top and left from the bot
+            var distanceTop = Math.abs(dumbBot.top - playerTop);
+            var distanceLeft = Math.abs(dumbBot.left - playerLeft);
+            //if first player, set distance and id in closestPlayer Object
+            if (closestPlayer.distance === undefined){
+                closestPlayer.distance = distanceTop + distanceLeft;
+                closestPlayer.number = i;
             } else {
-                //if next player is closest : reset distanceTop.value and distanceTop.id
-                distanceTemp = Math.abs(dumbBot.top - playerTop);
-                if (distanceTemp < closestTop.dist){
-                    closestTop.dist = distanceTemp;
-                    closestTop.id = i;
-                }
-            }
-
-        })
-        //left
-        playerLeft.forEach(function(playerLeft, i){
-            //first player : set distanceTop.value and distanceTop.id
-            if (closestLeft.dist === undefined){
-                closestLeft.dist = Math.abs(dumbBot.left - playerLeft);
-                closestLeft.id = i;
-            } else {
-                //if next player is closest : reset distanceTop.value and distanceTop.id
-                distanceTemp = Math.abs(dumbBot.left - playerLeft);
-                if (distanceTemp < closestLeft.dist){
-                    closestLeft.dist = distanceTemp;
-                    closestLeft.id = i;
+                //if next player is closest, reset distance and id in closestPlayer Object
+                distanceTemp = distanceTop + distanceLeft;;
+                if (distanceTemp < closestPlayer.distance){
+                    closestPlayer.distance = distanceTemp;
+                    closestPlayer.number = i;
                 }
             }
         })
 
-        //find closest player id
-        if (closestLeft.dist < closestTop.dist){
-            var closestPlayerId = closestLeft.id;
-        } else {
-            var closestPlayerId = closestTop.id;
-        }
-
-        //closest Player Top
-        var closestPlayer = document.querySelector("#player"+closestPlayerId);
+        //get closest player from id and get its top and left coords
+        var closestPlayer = document.querySelector("#player"+closestPlayer.number);
         var closestPlayerTop = removePxParseInt(closestPlayer.style.top)
         var closestPlayerLeft = removePxParseInt(closestPlayer.style.left)
 
-
+        //make bot move y axis
         if (dumbBot.top > closestPlayerTop){
-
             var goBot = dumbBot.moveVerification("top");
             if (goBot.move){
                 var newCoords = dumbBot.go(goBot.id, goBot.property, goBot.coordToApply, goBot.targetReach);
@@ -86,8 +47,7 @@ function moveDumbBot(){
             }
         }
 
-
-
+        //make bot move x axis
         if (dumbBot.left > closestPlayerLeft){
             var goBot = dumbBot.moveVerification("left");
             if (goBot.move){
@@ -101,79 +61,6 @@ function moveDumbBot(){
                 dumbBot.left = removePxParseInt(newCoords["left"]);
             }
         }
-
-
-
-        // //set "go" directions for each dumbbot
-        // if(go["top"][dumbBot.id] === undefined){
-        //     go["top"][dumbBot.id] = false;
-        // }
-        // if(go["bottom"][dumbBot.id] === undefined){
-        //     go["bottom"][dumbBot.id] = false;
-        // }
-        // if(go["left"][dumbBot.id] === undefined){
-        //     go["left"][dumbBot.id] = false;
-        // }
-        // if(go["right"][dumbBot.id] === undefined){
-        //     go["right"][dumbBot.id] = true;
-        // }
-        //
-        // //Set relative directions
-        // switch (true) {
-        //     case go["right"][dumbBot.id] :
-        //     var forward = "right";
-        //     var left = "top";
-        //     var right = "bottom";
-        //     break;
-        //
-        //     case go["left"][dumbBot.id] :
-        //     var forward = "left";
-        //     var left = "bottom";
-        //     var right = "top";
-        //     break;
-        //
-        //     case go["bottom"][dumbBot.id] :
-        //     var forward = "bottom";
-        //     var left = "right";
-        //     var right = "left";
-        //     break;
-        //
-        //     case go["top"][dumbBot.id] :
-        //     var forward = "top";
-        //     var left = "left";
-        //     var right = "right";
-        //     break;
-        //     default:
-        //
-        // }
-        // console.log("forward : " + forward);
-        // if(go[forward][dumbBot.id]){
-        //     //check if there is a path @ right
-        //     var pathRight = dumbBot.moveVerification(right);
-        //     //if yes
-        //     if (pathRight.move){
-        //         console.log("path right");
-        //         //turn right
-        //         go[forward][dumbBot.id] = false;
-        //         go[right][dumbBot.id] = true;
-        //         //go
-        //         dumbBot.go(pathRight.id, pathRight.property, pathRight.coordToApply, pathRight.targetReach);
-        //     } else {
-        //         console.log("no path right");
-        //         //check if there is a path forward
-        //         var pathForward = dumbBot.moveVerification(forward);
-        //         //if yes
-        //         if (pathForward.move){
-        //             //go
-        //             dumbBot.go(pathForward.id, pathForward.property, pathForward.coordToApply, pathForward.targetReach);
-        //         } else {
-        //             //turn left
-        //             go[forward][dumbBot.id] = false;
-        //             go[left][dumbBot.id] = true;
-        //         }
-        //     }
-        // }
-
 
     }
 )}
