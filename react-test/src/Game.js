3bf4beeb1510176642439fcd4baf6a0a760d78d2
Benoît Ripoche <ripoche.b@gmail.com>
2017-05-19 01:02:57 +0200

    toggle class test

--- react-test/src/Game.js
+++ react-test/src/Game.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import css from "./../styles/app.css"
 
-/**
- * A counter button: tap the button to increase the count.
- */
 class Game extends React.Component {
     constructor() {
         super();
         this.state = {
             count: 0,
+            style1:{
+                background:"red",
+                width: "150px",
+                height: "50px",
+                padding: "5px",
+                borderRadius: "5px",
+            },
+            style2:{
+                background:"blue",
+                width: "50px",
+                height: "150px",
+                padding: "5px",
+                borderRadius: "5px",
+            },
+            isStyle1: true,
         };
     }
 
@@ -17,18 +29,31 @@ class Game extends React.Component {
         <div>
             <h1>This is a test</h1>
             <div
-                style={{
-                    background:"red",
-                    width: "150px",
-                    padding: "5px",
-                    borderRadius: "5px",
-                }}
+                style={this.state.style1}
                 onClick={(e) => {
                     this.setState({ count: this.state.count + 5 });
-                    e.target.style.background = "blue";
+                    if(this.state.isStyle1){
+                        e.target.style.background = this.state.style2.background;
+                        e.target.style.width = this.state.style2.width;
+                        e.target.style.height = this.state.style2.height;
+                        this.setState({isStyle1: false})
+                    } else {
+                        e.target.style.background = this.state.style1.background;
+                        e.target.style.width = this.state.style1.width;
+                        e.target.style.height = this.state.style1.height;
+                        this.setState({isStyle1: true})
+                    }
+
                 }}
             >
-                <p>test</p>
+                <p>0</p>
+                <p>0</p>
+                <p>0</p>
+                <p>0</p>
+                <p>0</p>
+                <p>0</p>
+                <p>0</p>
+                <p>0</p>
                 Count: {this.state.count}
             </div>
         </div>
